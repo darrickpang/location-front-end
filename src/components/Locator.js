59e4098d4cdf10b48cb134ff9b5ecbe847fdca5a
darrickpang <dkypang@yahoo.com>
2021-02-02 16:13:01 -0800

    locator

---
+++ src/components/Locator.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Locator extends React.Component{
+    render(){
+        return(
+            <div>
+                Locator.js
+            </div>
+        )
+    }
+}
+
+export default Locator 
