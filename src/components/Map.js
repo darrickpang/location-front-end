b916658edfa40cdaac7e9e97b4ec41044d73031b
darrickpang <dkypang@yahoo.com>
2021-02-07 14:34:30 -0800

    maps

--- src/components/Map.js
+++ src/components/Map.js
@@ -45,29 +45,73 @@ export class CurrentLocation extends React.Component {
 
     componentDidMount() {
         if (this.props.centerAroundCurrentLocation) {
-          if (navigator && navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(pos => {
-              const coords = pos.coords;
-              this.setState({
-                currentLocation: {
-                  lat: coords.latitude,
-                  lng: coords.longitude
-                }
-              });
-            });
-          }
+            if (navigator && navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(pos => {
+                    const coords = pos.coords;
+                    this.setState({
+                        currentLocation: {
+                            lat: coords.latitude,
+                            lng: coords.longitude
+                        }
+                    });
+                });
+            }
         }
-        this.loadMap();<^>
-      }
+        this.loadMap();
+    }
+
+    loadMap() {
+        if (this.props && this.props.google) {
+            // checks if google is available
+            const { google } = this.props;
+            const maps = google.maps;
+    
+            const mapRef = this.refs.map;
+    
+            // reference to the actual DOM element
+            const node = ReactDOM.findDOMNode(mapRef);
+    
+            let { zoom } = this.props;
+            const { lat, lng } = this.state.currentLocation;
+            const center = new maps.LatLng(lat, lng);
+    
+            const mapConfig = Object.assign(
+                {},
+                {
+                    center: center,
+                    zoom: zoom
+                }
+            );
+    
+            // maps.Map() is constructor that instantiates the map
+            this.map = new maps.Map(node, mapConfig);
+        }
+    }
+
+    renderChildren() {
+        const { children } = this.props;
+    
+        if (!children) return;
+    
+        return React.Children.map(children, c => {
+            if (!c) return;
+    
+            return React.cloneElement(c, {
+                map: this.map,
+                google: this.props.google,
+                mapCenter: this.state.currentLocation
+            });
+        });
+    }
 
     render() {
         const style = Object.assign({}, mapStyles.map);
+    
         return (
-            <div id="map">
+            <div>
                 <div style={style} ref="map">
                     Loading map...
                 </div>
-                <MyMapComponent currentLocation={this.state.currentLocation}/>
                 {this.renderChildren()}
             </div>
         );
